4c736b9d9047f9bced0f557653a6a587f731db45
AnkitGrin <workforgrin@gmail.com>
2024-05-23 20:26:55 +0530

    about section updated

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,25 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        After dropping out from a <span className="font-medium">BCA</span>{" "}
+        degree, I decided to pursue my passion for programming. I learned{" "}
+        <span className="font-medium">front-end development</span> from youtube
+        & udemy courses.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
+        is <span className="font-medium">React, Next.js, and Three.js</span>. I
+        am also familiar with TypeScript and Framer motion. I am always looking
+        to learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a front-end
         developer.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        chess, watching movies, and chilling out with my friends. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        learning about <span className="font-medium">philosophy</span>.
       </p>
     </motion.section>
   );
